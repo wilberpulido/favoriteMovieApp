0ed253f7cbec2a069d7b91e7a9241804673bbd60
wilberpulido <pulidowilber@gmail.com>
2021-03-12 00:01:48 -0300

    Bases para la app

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/pages/Home.module.scss'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico"/>
       </Head>
 
       <main className={styles.main}>
