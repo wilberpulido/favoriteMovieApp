23ea7e8c6086410a38fea49bb394e90c7f1c0114
wilberpulido <pulidowilber@gmail.com>
2021-05-01 20:29:01 -0300

    create componenet formRegister and style

--- components/Form.js
+++ components/Form.js
@@ -1,10 +1,10 @@
 import style from '../styles/components/form.module.scss'
 import HeaderForm from './HeaderForm'
 
-export default function Form ({ titleForm, classNameForm, classNameContentForm, iconSrc, children }) {
+export default function Form ({ titleForm, classNameForm, classNameContentForm, iconSrc, handlerSubmitan, children }) {
   if (titleForm) {
     return (
-      <form className={style[classNameForm]}>
+      <form className={style[classNameForm]} onSubmit={handlerSubmitan}>
         <HeaderForm iconSrc={iconSrc}>
           {titleForm}
         </HeaderForm>
